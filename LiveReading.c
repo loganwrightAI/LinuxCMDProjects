fa65f280e8fac58a554c989bfb6e7cdbe5fbbbb2
Logan Wright <wri19003@byui.edu>
2023-08-17 01:40:21 -0700

    Added a new file; I started a new personal project to mess with settings of live data tracking over serial ports in the terminal. Just started out.

---
+++ LiveReading.c
@@ -0,0 +1,17 @@
+/*
+
+This is a project to display live readings of sensor data incoming from an Arduino
+through serial comm (USB).
+
+*/
+
+// Include all of the dependencies
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ncurses.h>
+
+
+int main(void) {
+	return 0.0;
+}
